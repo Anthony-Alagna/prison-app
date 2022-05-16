95e31c03cec2651fd25e4c948130899cef7149a1
Anthony Alagna <alalagna@uwm.edu>
2022-05-16 04:46:19 +0000

    add a bunch of people and orgs, link in graph component

--- src/Components/NetworkGraph/GraphComponent.tsx
+++ src/Components/NetworkGraph/GraphComponent.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Graph from "react-graph-vis";
 import { org, person } from "../../Types/index"
+
+
 import ChuckGrassley from '../../../img/People/ChuckGrassley.png'
 import DavidPerdue from '../../../img/People/DavidPerdue.png'
 import MissingPng from '../../../img/missing_png.png'
-import GEOGroup from '../../../img/Organizations/GEOGroup.png'
+import CoryGardner from '../../../img/People/CoryGardner.png'
+import MitchMcConnell from '../../../img/People/MitchMcConnell.png'
+import CapitoShelleyMoore from '../../../img/People/MitchMcConnell.png'
+import JohnCornyn from '../../../img/People/JohnCornyn.png'
+import HenryCuellar from '../../../img/People/HenryCuellar.png'
+import BillHagerty from '../../../img/People/BillHagerty.png'
 
+
+import GEOGroup from '../../../img/Organizations/GEOGroup.png'
+import ApolloGlobalMgmt from '../../../img/Organizations/ApolloGlobalMgmt.png'
 
 
 
@@ -37,9 +47,69 @@ const NetworkGraph = () => {
                 brokenImage: MissingPng,
                 label: "GEO Group",
             },
+            {
+                id: 4,
+                shape: "circularImage",
+                image: CoryGardner, //55979 donated from GEO Group
+                brokenImage: MissingPng,
+                label: "Cory Gardner",
+            },
+            {
+                id: 5,
+                shape: "circularImage",
+                image: MitchMcConnell, // GEOGroup donated 52204
+                brokenImage: MissingPng,
+                label: "Mitch McConnel",
+            },
+            {
+                id: 6,
+                shape: "circularImage",
+                image: CapitoShelleyMoore, //GEOGroup donated 46000
+                brokenImage: MissingPng,
+                label: "Shelley Moore Capito",
+            },
+            {
+                id: 7,
+                shape: "circularImage",
+                image: JohnCornyn,  //GEOGroup donated 33600
+                brokenImage: MissingPng,
+                label: "John Cornyn",
+            },
+            {
+                id: 8,
+                shape: "circularImage",
+                image: HenryCuellar, //GEOGroup donated 39400
+                brokenImage: MissingPng,
+                label: "Henry Cuellar",
+            },
+            {
+                id: 9,
+                shape: "circularImage",
+                image: BillHagerty, //Appollo Global Management donated 52235
+                brokenImage: MissingPng,
+                label: "Bill Hagerty",
+            },
+            {
+                id: 10,
+                shape: "circularImage",
+                image: ApolloGlobalMgmt,
+                brokenImage: MissingPng,
+                label: "Apollo Global Management",
+            },
+
+
         ],
         edges: [
-            { from: 3, to: 2},
+            { from: 3, to: 2 },
+            { from: 3, to: 4 },
+            { from: 3, to: 5 },
+            { from: 3, to: 6 },
+            { from: 3, to: 7 },
+            { from: 3, to: 8 },
+            { from: 10, to: 9 },
+            { from: 10, to: 1 },
+            { from: 10, to: 3 },
+
         ]
     };
 
@@ -69,9 +139,9 @@ const NetworkGraph = () => {
             graph={graph}
             options={options}
             getNetwork={network => {
-                network.on("click", function(params) {
-                      
-                    
+                network.on("click", function (params) {
+
+
                 });
 
                 //  if you want access to vis.js network api you can set the state in a parent component using this property
